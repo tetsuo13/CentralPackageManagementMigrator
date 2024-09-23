f9e59b2614f2981af7647377454925c4fbb9bc24
Andrei Nicholson <contact@andreinicholson.com>
2024-09-22 21:22:18 -0400

    Note for future self: flushing is important

--- src/CentralPackageManagementMigrator/LoggingUtility.cs
+++ src/CentralPackageManagementMigrator/LoggingUtility.cs
@@ -2,6 +2,9 @@ using Microsoft.Extensions.Logging;
 
 namespace CentralPackageManagementMigrator;
 
+/// <summary>
+/// Manual logging management utility.
+/// </summary>
 internal static class LoggingUtility
 {
     private static ILoggerFactory? _loggerFactory;
@@ -17,7 +20,6 @@ internal static class LoggingUtility
         {
             builder.AddSimpleConsole(options =>
             {
-                // TODO: Disable scopes or something
                 options.IncludeScopes = false;
                 options.SingleLine = true;
                 options.TimestampFormat = "HH:mm:ss ";
@@ -27,6 +29,11 @@ internal static class LoggingUtility
         });
     }
 
+    /// <summary>
+    /// Manually call after all logging statements in order to flush any
+    /// existing logs that haven't been output yet. Failure to call this
+    /// method may result in lost messages.
+    /// </summary>
     public static void FlushLogging() => Factory?.Dispose();
 
     public static ILogger<T> CreateLogger<T>() => Factory.CreateLogger<T>();
