7f1de19a0db38739312ca4aa37aebd0f69631062
Andrei Nicholson <contact@andreinicholson.com>
2026-03-12 20:46:30 -0400

    Add some more basic tests

--- tests/CentralPackageManagementMigrator.Tests/MigratorCommandTests.cs
+++ tests/CentralPackageManagementMigrator.Tests/MigratorCommandTests.cs
@@ -1,3 +1,5 @@
+using System.CommandLine;
+using System.CommandLine.Help;
 using System.Linq;
 using Microsoft.Extensions.Logging;
 using Xunit;
@@ -6,31 +8,89 @@ namespace CentralPackageManagementMigrator.Tests;
 
 public class MigratorCommandTests
 {
-    private const string LogLevelOptionName = "--verbosity";
-
     [Fact]
-    public void Constructor_ShouldInitialize_VerbosityOptionByName()
+    public void Command_Options_ContainsHelpDescriptionVerbosity()
     {
-        var command = new MigratorCommand();
-        Assert.Contains(command.Options, option => option.Name == LogLevelOptionName);
+        var command = CreateCommand();
+
+        Assert.Equal(3, command.Options.Count);
+        Assert.Single(command.Options.OfType<HelpOption>());
+        Assert.Single(command.Options.OfType<VersionOption>());
+        Assert.Single(command.Options.OfType<Option<LogLevel>>());
     }
 
     [Fact]
-    public void Constructor_ShouldInitialize_VerbosityOptionByAlias()
+    public void VerbosityOption_Contains_Aliases()
     {
-        var command = new MigratorCommand();
+        var command = CreateCommand();
+        var verbosityOptions = GetVerbosityOption(command);
 
-        var logLevelOption = command.Options.Single(option => option.Name == LogLevelOptionName);
-        Assert.Single(logLevelOption.Aliases);
-        Assert.Equal("-v", logLevelOption.Aliases.First());
+        Assert.Equal("--verbosity", verbosityOptions.Name);
+        Assert.Single(verbosityOptions.Aliases);
+        Assert.Equal("-v", verbosityOptions.Aliases.Single());
     }
 
     [Fact]
-    public void Constructor_ShouldInitialize_DefaultVerbosityOption()
+    public void VerbosityOptions_Contains_Description()
     {
-        var command = new MigratorCommand();
+        var command = CreateCommand();
+        var verbosityOption = GetVerbosityOption(command);
 
-        var logLevelOption = command.Options.Single(option => option.Name == LogLevelOptionName);
-        Assert.Equal(LogLevel.Information, logLevelOption.GetDefaultValue());
+        Assert.NotNull(verbosityOption.Description);
+        Assert.Contains("Verbosity", verbosityOption.Description);
     }
+
+    [Fact]
+    public void Parse_UnknownOption_ReportsError()
+    {
+        var command = CreateCommand();
+        var parseResult = command.Parse(["--unknown-option-doesnt-exist", nameof(Parse_UnknownOption_ReportsError)]);
+
+        // The first arg isn't recognized as a valid option so the second
+        // isn't recognized as the value for the first arg. Instead, they both
+        // should be flagged as invalid.
+        Assert.Equal(2, parseResult.Errors.Count);
+    }
+
+    [Fact]
+    public void Parse_NoArguments_UsesInformationVerbosity()
+    {
+        var command = CreateCommand();
+        var verbosityOption = GetVerbosityOption(command);
+
+        var parseResult = command.Parse([]);
+
+        Assert.Empty(parseResult.Errors);
+        Assert.Equal(LogLevel.Information, parseResult.GetValue(verbosityOption));
+    }
+
+    [Theory]
+    [InlineData("--verbosity", "Debug", LogLevel.Debug)]
+    [InlineData("--verbosity", "Warning", LogLevel.Warning)]
+    [InlineData("--verbosity", "None", LogLevel.None)]
+    [InlineData("-v", "Warning", LogLevel.Warning)]
+    public void Parse_Verbosity_ReturnsLogLevel(string flag, string flagValue, LogLevel expectedLevel)
+    {
+        var command = CreateCommand();
+        var verbosityOption = GetVerbosityOption(command);
+
+        var parseResult = command.Parse([flag, flagValue]);
+
+        Assert.Empty(parseResult.Errors);
+        Assert.Equal(expectedLevel, parseResult.GetValue(verbosityOption));
+    }
+
+    [Fact]
+    public void Parse_VerbosityInvalid_ReportsError()
+    {
+        var command = CreateCommand();
+        var parseResult = command.Parse(["--verbosity", nameof(Parse_VerbosityInvalid_ReportsError)]);
+
+        Assert.Single(parseResult.Errors);
+    }
+
+    private static MigratorCommand CreateCommand() => [];
+
+    private static Option<LogLevel> GetVerbosityOption(MigratorCommand command) =>
+        command.Options.OfType<Option<LogLevel>>().Single();
 }
